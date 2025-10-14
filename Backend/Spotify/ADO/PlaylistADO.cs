831ac0cb6197afefd20a97d11601adf7cada157c
mmorerap <marcmoreraprat@gmail.com>
2025-10-14 18:53:12 +0200

    Marc - PlaylistADO Completed

--- Backend/Spotify/ADO/PlaylistADO.cs
+++ Backend/Spotify/ADO/PlaylistADO.cs
@@ -1,9 +1,9 @@
 using Microsoft.Data.SqlClient;
 using static System.Console;
-using dbdemo.Services;
-using dbdemo.Model;
+using Spotify.Services;
+using Spotify.Model;
 
-namespace dbdemo.Repository;
+namespace Spotify.Repository;
 
 class PlaylistADO
 {
@@ -42,62 +42,60 @@ class PlaylistADO
         dbConn.Close();
     }
 
-    public static List<Product> GetAll(DatabaseConnection dbConn)
+    public static List<Playlist> GetAll(DatabaseConnection dbConn)
     {
-        List<Product> products = new();
+        List<Playlist> playlist = new();
 
         dbConn.Open();
-        string sql = "SELECT Id, Code, Name, Price FROM Products";
+        string sql = "SELECT Id, Name,User_Id FROM Playlist";
 
         using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
         using SqlDataReader reader = cmd.ExecuteReader();
 
         while (reader.Read())
         {
-            products.Add(new Product
+            playlist.Add(new Product
             {
                 Id = reader.GetGuid(0),
-                Code = reader.GetString(1),
                 Name = reader.GetString(2),
-                Price = reader.GetDecimal(3)
+                User_Id = reader.GetDecimal(3)
             });
         }
 
         dbConn.Close();
-        return products;
+        return playlist;
     }
 
-    public static Product? GetById(DatabaseConnection dbConn, Guid id)
+    public static Playlist? GetById(DatabaseConnection dbConn, Guid id)
     {
         dbConn.Open();
-        string sql = "SELECT Id, Code, Name, Price FROM Products WHERE Id = @Id";
+        string sql = "SELECT Id, Name, User_Id FROM Playlist WHERE Id = @Id";
 
         using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
         cmd.Parameters.AddWithValue("@Id", id);
 
         using SqlDataReader reader = cmd.ExecuteReader();
-        Product? product = null;    // Si no inicialitzem la variable => no existeix en el return!
+        Playlist? playlist = null;    // Si no inicialitzem la variable => no existeix en el return!
 
         if (reader.Read())
         {
-            product = new Product
+            playlist = new Playlist
             {
                 Id = reader.GetGuid(0),
-                Code = reader.GetString(1),
                 Name = reader.GetString(2),
-                Price = reader.GetDecimal(3)
+                User_Id = reader.GetDecimal(3)
             };
         }
 
         dbConn.Close();
-        return product;
+        return playlist;
     }
 
     public static bool Delete(DatabaseConnection dbConn, Guid id)
     {
         dbConn.Open();
 
-        string sql = @"DELETE FROM Products WHERE Id = @Id";
+        string sql = @"DELETE FROM Playlist WHERE Id = @Id";
 
         using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
         cmd.Parameters.AddWithValue("@Id", id);
