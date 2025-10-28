d1ea73ff5a7ceb22a9141f916df361fdc938522e
mmorerap <marcmoreraprat@gmail.com>
2025-10-28 17:36:08 +0100

    Crear Model RolesPermissions

---
+++ Backend/Spotify/models/RolesPermissions.cs
@@ -0,0 +1,12 @@
+namespace Spotify.Model;
+
+public class RolePermission
+{
+    public Guid Id { get; set; }
+    
+    public string Permission_Code { get; set; }
+    
+    public string Role_Code { get; set; }   
+
+    
+}
