69de1583a944c90b3ff0872697a1f7d88e8961cf
mmorerap <marcmoreraprat@gmail.com>
2025-10-28 17:04:19 +0100

    Creacio del Model Permissions

---
+++ Backend/Spotify/models/Permission.cs
@@ -0,0 +1,10 @@
+namespace Spotify.Model;
+
+public class Playlist
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public Guid User_Id { get; set; }   
+}
