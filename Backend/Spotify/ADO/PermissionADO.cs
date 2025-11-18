e70c5c08d54b0c010520190b83ff1a4bbdaf57ba
mmorerap <marcmoreraprat@gmail.com>
2025-11-18 15:45:49 +0100

    Changes Model Permissions + creation PermissionADO

---
+++ Backend/Spotify/ADO/PermissionADO.cs
@@ -0,0 +1,32 @@
+using Microsoft.Data.SqlClient;
+using Spotify.Model;
+using Spotify.Services;
+
+
+namespace Spotify.Repository;
+
+class PermissionADO
+{
+    public static List<Permission> GetAll(DatabaseConnection dbConn)
+    {
+        List<Permission> permissions = new();
+
+        dbConn.Open();
+        string sql = "SELECT ID, Code, Name FROM Permissions";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        using SqlDataReader reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            permissions.Add(new Role
+            {
+                Id = reader.GetGuid(0),
+                Code = reader.GetString(1),
+                Name = reader.GetString(2),
+            });
+        }
+        dbConn.Close();
+        return permissions;
+    }
+}
--- Backend/Spotify/models/Permission.cs
+++ Backend/Spotify/models/Permission.cs
@@ -4,7 +4,7 @@ public class Permission
 {
     public Guid Id { get; set; }
     
-    public string Name { get; set; }
+    public string Code { get; set; }
     
-    public Guid User_Id { get; set; }   
+    public string Name { get; set; }   
 }
