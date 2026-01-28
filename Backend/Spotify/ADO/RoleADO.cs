55a09f2830bea2be060f44f77c6a17f87c9b6646
mmorerap <marcmoreraprat@gmail.com>
2026-01-28 16:58:09 +0100

    Refactor i creacio Role ADO

--- Backend/Spotify/ADO/RoleADO.cs
+++ Backend/Spotify/ADO/RoleADO.cs
@@ -7,6 +7,7 @@ namespace Spotify.Repository;
 
 class RoleADO
 {
+    //GET
     public static List<Role> GetAll(DatabaseConnection dbConn)
     {
         List<Role> roles = new();
@@ -29,4 +30,65 @@ class RoleADO
         dbConn.Close();
         return roles;
     }
+
+    //POST
+    public static void Insert(DatabaseConnection dbConn,Role role)
+    {
+
+        dbConn.Open();
+
+        string sql = @"INSERT INTO Roles (Id, Code, Name)         
+                        VALUES (@Id, @Code, @Name)";            
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", role.Id);
+        cmd.Parameters.AddWithValue("@Code", role.Code);
+        cmd.Parameters.AddWithValue("@Name", role.Name);
+
+        cmd.ExecuteNonQuery();
+
+        dbConn.Close();
+    }
+
+    //UPDATE
+    public static void Update(DatabaseConnection dbConn, Role role)
+    {
+        dbConn.Open();
+
+        string sql = @"UPDATE Roles 
+                        SET
+                        Id = @Id,
+                        Code = @Code,
+                        Name = @Name
+                        WHERE Id = @Id";
+                        
+
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", role.Id);
+        cmd.Parameters.AddWithValue("@Nom", role.Code);
+        cmd.Parameters.AddWithValue("@Descripcio", role.Name);
+
+        cmd.ExecuteNonQuery();
+    
+        dbConn.Close();
+    }
+
+    //DELETE
+     public static bool Delete(DatabaseConnection dbConn, Guid Id)
+    {
+        dbConn.Open();
+
+        string sql = @"DELETE FROM Roles WHERE Id = @Id";
+
+        using SqlCommand cmd = new SqlCommand(sql, dbConn.sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", Id);
+
+        int rows = cmd.ExecuteNonQuery();
+
+        dbConn.Close();
+
+        return rows > 0;
+    }
+
 }
